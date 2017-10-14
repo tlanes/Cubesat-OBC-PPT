fafc6be9b13d9239ed1b20631021fe6f1e20db01
tlanes <tlanes@asu.edu>
2017-10-14 16:22:23 -0700

    M/S
    Master and Slave codes

---
+++ MASTER.ino
@@ -0,0 +1,51 @@
+//MASTER
+#include <Wire.h>
+
+const byte MY_ADDRESS = 25;
+const byte SLAVE_ADDRESS = 42;
+const byte LED = 13;
+
+void setup () 
+  {
+  Serial.begin(9600);
+  Wire.begin (MY_ADDRESS);
+  pinMode (LED, OUTPUT);   
+  Wire.onReceive(receiveEvent);  
+  }  // end of setup
+
+void loop () 
+  {
+  while (Serial.available()){
+   byte x = Serial.read();
+   Wire.beginTransmission(SLAVE_ADDRESS);
+   Wire.write(x); 
+   if (Wire.endTransmission () == 0)
+    Serial.println("tx success");
+   else
+    Serial.println("tx fail");
+   Wire.requestFrom(SLAVE_ADDRESS, 1);
+   int bytes = Wire.available();
+   Serial.print("Slave sent ");
+   Serial.print(bytes);
+   Serial.print(" of information\n");
+   for(int i = 0; i< bytes; i++)
+   {
+    x = Wire.read();
+    Serial.print("Slave Sent: ");
+    Serial.print(x, HEX);
+    Serial.print("\n");
+  }
+  }
+  delay(500);
+  }  // end of loop
+
+
+void receiveEvent (int howMany)
+ {
+  for (int i = 0; i < howMany; i++)
+    {
+    byte b = Wire.read ();
+    Serial.println (b); 
+    }  // end of for loop
+} // end of receiveEvent
+
